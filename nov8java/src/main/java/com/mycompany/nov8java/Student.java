627ba36f1827791e2f037f8daccc51e08a48a262
sweta09876 <116981671+sweta09876@users.noreply.github.com>
2022-11-08 23:18:57 -0500

    initial commit

---
+++ nov8java/src/main/java/com/mycompany/nov8java/Student.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.nov8java;
+
+/**
+ *
+ * @author Sweta
+ */
+public class Student {
+  private int id;
+private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+}
---
+++ nov8java/src/test/java/test/com/StudentTest.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+
+package test.com;
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author Sweta
+ */
+public class StudentTest {
+
+    public StudentTest() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+}
